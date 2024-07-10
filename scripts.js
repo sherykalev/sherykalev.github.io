f33b0ae9073ea32f2620b1350bc83461d00db7dc
oren <oren@gmai.com>
2024-07-10 17:19:09 +0300

    feat: stop slide show autoplay onclick

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 let slideIndex = 0;
+let autoShow = true;
 showSlides();
 
 function showSlides() {
+    if (!autoShow) {
+        return;
+    }
     let i;
     let slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
@@ -14,7 +18,15 @@ function showSlides() {
 }
 
 function plusSlides(n) {
-    slideIndex += n - 1;
-    showSlides();
+    autoShow = false;
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex += n;
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    if (slideIndex < 0) {slideIndex = slides.length-1}
+    slides[slideIndex].style.display = "block";
 }
 
