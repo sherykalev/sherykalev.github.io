aac97dda8cad73bafba778b75e641c1bed644ef2
oren <oren@gmai.com>
2024-07-11 20:03:56 +0300

    feat: font to roboto, confetti animation

--- scripts.js
+++ scripts.js
@@ -30,3 +30,27 @@ function plusSlides(n) {
     slides[slideIndex].style.display = "block";
 }
 
+
+function triggerConfetti() {
+    var duration = 1 * 200;
+    var end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+        particleCount: 5,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 }
+        });
+        confetti({
+        particleCount: 5,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+        requestAnimationFrame(frame);
+        }
+    })();
+}
